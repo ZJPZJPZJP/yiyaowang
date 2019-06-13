c966d5711f9f8017e9bf5a8e690658231a73d333
zjp <2216032502@qq.com>
2019-06-13 21:20:32 +0800

    第二次上传

--- yiyaowang/js/yyw.js
+++ yiyaowang/js/yyw.js
@@ -21,6 +21,47 @@
 			})
 			
 		
+			var $elevator_n=$('.elevator_n')
+			var $in_floor_l=$('.in_floor_l')
+			$(window).on('scroll',function(){
+					//1.通过滚动条的距离控制楼梯导航的显示与隐藏
+					var $top=$(window).scrollTop();
+					
+					if($top>=650&&$top<5500){
+						$elevator_n.show();
+					}else{
+						$elevator_n.hide();
+					}
+			
+			$('.in_floor_l').each(function(index,element){
+						var $loucengtop=$(element).offset().top+300;
+						if($loucengtop>$top){
+							$('.elevator_n li').eq($(this).index()).addClass('current hove');
+							console.log
+						}
+							
+				})
+			})	
+				$('.elevator_n li').on('mouseover',function(){
+					$(this).addClass('hover').siblings('li').removeClass('hover');
+				})
+				$('.elevator_n li').on('mouseout',function(){
+					$(this).removeClass('hover');
+				})
+		
+				$('.elevator_n li').on('click',function(){
+					
+					$(this).addClass('current hove').siblings('li').removeClass('current hove');
+					var $top=$('.in_floor_l').eq($(this).index()).offset().top;
+					$('html,body').animate({//这里给滚动条赋值，兼容写法
+			    		scrollTop:$top
+			    	});
+				});
+			
+			
+			
+			
+	
 		
 		
 		
